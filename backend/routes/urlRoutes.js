cfa7878c7ad566deb4f136199727328be3515ec2
venu-04 <kamsuvenu2004@gmail.com>
2024-10-27 09:25:09 +0530

    routes changed

--- backend/routes/urlRoutes.js
+++ backend/routes/urlRoutes.js
@@ -2,7 +2,11 @@ import express from 'express';
 import {shortenUrl,redirectUrl} from '../controllers/urlcontroller.js';
 const router=express.Router();
 
-router.post('/shorten',shortenUrl);
+router.post("/shorten",shortenUrl);
+router.get('/shorten',(req,res)=>{
+    console.log('hello');
+    res.send('hello');
+});
 router.get('/:shortUrl',redirectUrl);
 
 export default router;
